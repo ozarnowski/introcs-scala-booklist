61479a8b4c03d32b1305d62fec7a2d8726ca6820
ozarnowski <ozarnowski@luc.edu>
2015-04-22 20:00:01 +0000

    done

--- booklist.scala
+++ booklist.scala
@@ -1,4 +1,4 @@
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.MutableList
 import scala.util.Try
 import scala.io.Source
 
@@ -10,44 +10,61 @@ case class Book(title : String, author : String, year : Int)
 // Your job is to replace all ??? with your own code.
 
 class BookList {
-   var list = ListBuffer[Book]()
+   var list = MutableList[Book]()
 
    def addBook(book : Book) : Unit = {
+       list += book
+       
       // write code to add the book to 'list'.
       // hint: Use the list add (+=) method
    }
 
    def getNumberOfBooks() : Int = list.length
-      // instructor has done this for you
+      
+    // instructor has done this for you
       // using it for testing purposes
 
    def printList() : Unit = {
+       list.foreach(println)
+       
       // write code to printList()
       // you should also show how to use the method in your main
    }
 
-   def getTitlesByAuthor(author : String) : ListBuffer[String] = {
-      val byAuthorList = ListBuffer[String]()
-      // return a list of all titles that are written by author
-
-
+   def getTitlesByAuthor(bookauthor : String) : MutableList[String] = {
+       val byAuthorList = MutableList[String]()
+       for (n <- list)
+            if (n.author == bookauthor){
+            var a = n.title
+            byAuthorList += a
+       }
       byAuthorList
+      // return a list of all titles that are written by author
    }
 
-   def getTitlesContaining(substring : String) : ListBuffer[String] = {
-      val titles = ListBuffer[String]()
+   def getTitlesContaining(substring : String) : MutableList[String] = {
+      val titles = MutableList[String]()
+       for (n <- list)
+       if (n.title == substring){
+           titles += n.title
+       } else if (n.author == substring){
+           titles += n.title
+       }
+       titles
       // return a list of all titles that contain a substring
-
-      titles
    }
 
-   def getBooksBetweenYears(firstYear : Int, lastYear : Int) : ListBuffer[Book] = {
-     
-      val betweenYearList = ListBuffer[Book]()
+   def getBooksBetweenYears(firstYear : Int, lastYear : Int) : MutableList[String] = {
+      val betweenYearList = MutableList[String]()
+       for (y <- firstYear to lastYear){
+           for (n <- list){
+               if (n.year == y){
+                   betweenYearList += n.title
+               }
+           }
+       }
+       betweenYearList
       // get all books between two years
-
-
-      betweenYearList
    }
 
    def addFromFile(name : String) : Unit = {
